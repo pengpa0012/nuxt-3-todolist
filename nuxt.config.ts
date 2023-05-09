f160fa1be8ed34ab7751b0ac1805b8b9fe4062a7
Pengpa0012 <peng.ilikha@gmail.com>
2023-05-09 21:06:36 +0800

    add tailwind module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   srcDir: "src/",
-  css: ['~/assets/css/main.css'],
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
+  modules: [
+    '@nuxtjs/tailwindcss'
+  ]
 })
